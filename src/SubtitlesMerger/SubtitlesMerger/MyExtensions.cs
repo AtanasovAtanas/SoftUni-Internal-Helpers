887dc6db2d3ed7cf9170c986e1a7458ec4e1dd4c
Atanas Atanasov <atanas4537@gmail.com>
2020-06-16 17:43:57 +0300

    rename Playground to SubtitlesMerger

---
+++ src/SubtitlesMerger/SubtitlesMerger/MyExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace SubtitlesMerger
+{
+    using System;
+
+    public static class MyExtensions
+    {
+        public static string ToSubtitleFormat(this TimeSpan ts)
+        {
+            var hours = ts.Hours;
+            var minutes = ts.Minutes;
+            var seconds = ts.Seconds;
+            var milliseconds = ts.Milliseconds;
+
+            return $"{hours}:{minutes:D2}:{seconds:D2}.{milliseconds:D3}";
+        }
+    }
+}
